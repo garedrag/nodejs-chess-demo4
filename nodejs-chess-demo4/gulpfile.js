18004590ad53595fb3de685f0901b6d855e4a312
Yaroslav <garedrag@gmail.com>
2019-09-11 16:23:38 +0300

    Delete gulpfile.js

--- nodejs-chess-demo4/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var path = require('path');
-var gulp = require('gulp');
-var tasksPath = path.join(__dirname, 'tasks');
-var tasks = fs.readdirSync(tasksPath);
-
-tasks.forEach(task => { require(path.join(tasksPath, path.basename(task, path.extname(task)))); });
-
-gulp.task('default', ['build']);
